8036e6334e46ce4ec225852e158a9c0edc3b9b67
Richard <richardmoa95@gmail.com>
2025-09-22 22:40:23 -0500

    First configure of Features

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
 // Add services to the container.
+builder.Services.AddCarterWithAssemblies(assemblies: assembly);
+
+builder.Services.AddMediatR(configuration: config =>
+{
+    config.RegisterServicesFromAssemblyContaining<Program>();
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
